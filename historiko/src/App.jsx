cba6d82a14a2a39ee54632a6a9a3ba3bdd1851b3
aipPlaton <alexplaton0607@gmail.com>
2024-08-17 01:25:05 +0800

    Second Commit

--- historiko/src/App.jsx
+++ historiko/src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import Profile from './components/Profile/Profile';
+import About from './components/About/About';
+import backgroundVideo from "./assets/hero.mp4";
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+        <video autoPlay loop muted id='video'><source src={backgroundVideo} type='video/mp4'/></video>
+        
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
