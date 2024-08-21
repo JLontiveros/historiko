6bc2b4d46500d597b00a22ab32ae2996960a544d
aipPlaton <alexplaton0607@gmail.com>
2024-08-22 00:25:07 +0800

    finished profile design

--- historiko/src/components/Profile/Profile.jsx
+++ historiko/src/components/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import './Profile.css';
 import girlicon from '../../assets/girlicon.png';
 import pen2 from '../../assets/pen2.png'
 import uploadarea from '../../assets/uploadareacropped.png'
+import badgge from '../../assets/badgge.png'
 
 const Profile = () => {
   const [isFormVisible, setFormVisible] = useState(false);
@@ -27,6 +28,61 @@ const Profile = () => {
           <button onClick={handleEditClick}>Edit<img src={pen2} alt='pen'/></button>
         </div>
       </div>
+
+      <div className="second-container">
+        <div className="gantimpala-container">
+          <h1>Mga Gantimpala</h1>
+          <div className="first">
+            <div className="gantimpalas">
+              <img src={badgge} className='badge'/>
+              <div className="info">
+                <span>Gantimpala para sa pag tatapos ng talakayan ng Labanan sa Tirad Pass</span>
+                <span>Date: 10/19/25</span>
+              </div>
+            </div>
+          </div>
+          <div className="second">
+            <div className="gantimpalas">
+              <img src={badgge} className='badge'/>
+              <div className="info">
+                <span>Sertipiko ng pag tatapos sa araling Panahon ng Digmaang Pilipino-Amerikano</span>
+                <span>Date: 10/25/25</span>
+              </div>
+            </div>
+          </div>
+          <div className="third">
+            <div className="gantimpalas">
+              <img src={badgge} className='badge'/>
+              <div className="info">
+                <span>Gantimpala para sa pag tatapos ng talakayan ng Balangiga Massacre</span>
+                <span>Date: 10/25/25</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="resulta-container">
+          <h1>Resulta</h1>
+          <div className="first">
+            <span>Sigaw ng Pugad-Lawin</span>
+          </div>
+          <div className="second">
+            <span>Tejeros Convention</span>
+          </div>
+          <div className="third">
+            <span>Balangiga Massacre</span>
+          </div>
+          <div className="fourth">
+            <span>Kasunduan sa Biak-na-Bato</span>
+          </div>
+          <div className="fifth">
+            <span>Unang Putok sa panukulan ng Silencio at Sociego, Sta Mesa</span>
+          </div>
+        </div>
+      </div>
+
+
+
+      {/* Edit Form */}
 
       <div className={`edit-form ${isFormVisible ? 'visible' : ''}`}>
           <form>
