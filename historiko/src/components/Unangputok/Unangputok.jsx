82c516e969ec0f029072d0cfef190e2f037f172e
aipPlaton <alexplaton0607@gmail.com>
2024-08-31 02:17:38 +0800

    added backend for TopicMarking

--- historiko/src/components/Unangputok/Unangputok.jsx
+++ historiko/src/components/Unangputok/Unangputok.jsx
@@ -95,6 +95,7 @@ const Unangputok = () => {
         .insert({
           user_id: userUUID,
           topic_id: topicId,
+          topic_name: topicName,
           marked_at: new Date().toISOString(),
           status: 'to_review'
         });
