4d205dd4da2c3144cdc9d89a4ce64b08d2673b9b
aipPlaton <alexplaton0607@gmail.com>
2024-09-07 03:16:22 +0800

    added responsiveness for modules, and two talakayin

--- historiko/src/components/Dalwatalakayin/Dalwatalakayin.jsx
+++ historiko/src/components/Dalwatalakayin/Dalwatalakayin.jsx
@@ -16,22 +16,26 @@ function Dalwatalakayin() {
           <img src={pugadlawin} alt="firstshot" className="event-icon" />
           <h2>Sigaw ng Pugad-Lawin</h2>
           <p>Ang mga kasapi ng katastaasan kagalanggalangang katipunan ng mga anak ng Bayan (KKK) o Katipunan ay sabay-sabay pinunit ang kani kanilang sedula bilang tanda ng kanilang pagtutol sa pamamahala ng mga espanyol.</p>
+            <div className="event-link">
+              <Link to="/sigaw" className="link-button">See more</Link>
+            </div>
         </div>
         <div className="event">
           <img src={tejeros2} alt="sword" className="event-icon" />
           <h2>Tejeros Convention</h2>
           <p>Ang kumbensiyon sa Tejeros na naganap noong Marso 22, 1897 ay isang pagpupulong sa Casa Hacienda de Tejeros, San Francisco de Malabon sa Cavvite na may layuning pagkasunduin ang dalwang paksiyon sa cavite.</p>
+            <div className="event-link">
+              <Link to="/tejeros" className="link-button">See more</Link>
+            </div>
         </div>
         <div className="event">
           <img src={biaknabato} alt="Bell" className="event-icon" />
           <h2>Kasunduan sa Biak-na-Bato</h2>
           <p>Biak na bato lugar na matatagpuan sa Miguel de mayumo, bulacan na nagsisilbing kuta nina Aguinaldo sa panahon ng pakikidigma nila sa mga espanyol</p>
+            <div className="event-link">
+              <Link to="/bato" className="link-button">See more</Link>
+            </div>
         </div>
-      </div>
-      <div className="eventbtns2">
-        <Link to="/sigaw" className="link-button">See more</Link>
-        <Link to="/tejeros" className="link-button">See more</Link>
-        <Link to="/bato" className="link-button">See more</Link>
       </div>
     </div>
   );
--- historiko/src/components/Unatalakayin/Unatalakayin.jsx
+++ historiko/src/components/Unatalakayin/Unatalakayin.jsx
@@ -16,23 +16,26 @@ function Unatalakayin() {
           <img src={plaquetteImage} alt="firstshot" className="event-icon" />
           <h2>Unang Putok sa panlulukan ng Silencio at Sociego, Sta.Mesa</h2>
           <p>Ang hindi pagkilala ng Estados Unidos sa Republika ng Pilipinas ang unang hudyat ng pagbabago sa pakikitungo ng mga amerikano sa mga Pilipino</p>
+            <div className="event-link">
+              <Link to="/unangputok" className="link-button">See more</Link>
+            </div>
         </div>
         <div className="event">
           <img src={tirad} alt="sword" className="event-icon" />
           <h2>Labanan sa Tirad Pass</h2>
           <p>Nangyari ang isa sa makasaysayang sagupaan ng mga rebolusyonaryong Pilipino, ang Labanan sa Pasong Tirad sa pangunguna ni Heneral Gregorio Del Pilar.</p>
+            <div className="event-link">
+              <Link to="/tiradpass" className="link-button">See more</Link>
+            </div>
         </div>
         <div className="event">
           <img src={balangiga} alt="Bell" className="event-icon" />
           <h2>Balangiga Massacre</h2>
-          <p>Pinakatanyag na labanan sa pagitan ng mga amerikano at mga Pilipino ay ang labanan sa balangiga na nangyari sa pamumuno ni Heneral Vicente Lukban sa Isla ng Samar.
-</p>
+          <p>Pinakatanyag na labanan sa pagitan ng mga amerikano at mga Pilipino ay ang labanan sa balangiga na nangyari sa pamumuno ni Heneral Vicente Lukban sa Isla ng Samar.</p>
+            <div className="event-link">
+              <Link to="/balangiga" className="link-button">See more</Link>
+            </div>
         </div>
-      </div>
-      <div className="eventbtns2">
-        <button><Link to="/unangputok" className="link-button">See more</Link></button>
-        <button><Link to="/tiradpass" className="link-button">See more</Link></button>
-        <button><Link to="/balangiga" className="link-button">See more</Link></button>
       </div>
     </div>
   );
