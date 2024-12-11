4ca684549de809aa068f7fb680db6df2331b7c56
Allen Eidrian Silva Ramos <115854514+llenny18@users.noreply.github.com>
2024-12-12 07:40:41 +0800

    all changes

--- historiko/src/supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://mqomhecazbpagsbfskzv.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1xb21oZWNhemJwYWdzYmZza3p2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQ2MDM5NjIsImV4cCI6MjA0MDE3OTk2Mn0.1zP8UxASY-wcTFtL8ln3jxzdnUsmn4L4DUQAq-edf2Q'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
